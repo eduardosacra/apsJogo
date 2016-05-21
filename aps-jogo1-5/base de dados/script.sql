63192fce599fa2d802bfaecf2f6de7633010f839
Eduardo S S <eduardo.sacra.sousa@gmail.com>
2016-05-21 12:38:27 -0300

    adicionado script para criar usuario e privilégios para manipular a base

--- aps-jogo1-5/base de dados/script.sql
+++ aps-jogo1-5/base de dados/script.sql
@@ -7,6 +7,9 @@ CREATE  TABLE `jma`.`recordes` (
   `nome` VARCHAR(45) NOT NULL ,
   `movimentos` INT NOT NULL ,
   PRIMARY KEY (`id`) );	
+  
+CREATE USER 'userjma'@'localhost' IDENTIFIED BY '123QwErT@';
+GRANT ALL PRIVILEGES ON jma.recordes TO 'userjma'@'localhost';
 
 drop table recordes;
 
@@ -14,10 +17,10 @@ insert into recordes (nome,movimentos) values('edu',25);
 insert into recordes (nome,movimentos) values('joca',35);
 insert into recordes (nome,movimentos) values('rick',36);
 insert into recordes (nome,movimentos) values('feh',22);
-insert into recordes (nome,movimentos) values('feh',24);
-insert into recordes (nome,movimentos) values('edu',98);
-insert into recordes (nome,movimentos) values('edu',120);
-insert into recordes (nome,movimentos) values('edu',123);
+insert into recordes (nome,movimentos) values('cah',24);
+insert into recordes (nome,movimentos) values('pai jorge',98);
+insert into recordes (nome,movimentos) values('mãe vó',120);
+insert into recordes (nome,movimentos) values('le',123);
 
 delete from recordes where recordes.id = all( select id from recordes where nome='edu');
 select  id,nome,movimentos from recordes where nome='edu' order by movimentos asc;
